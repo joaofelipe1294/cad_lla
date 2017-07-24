90d00b473e9277bc60df96c9af6127a25eea6879
joao_felipe <joaofelipe1294@gmail.com>
2017-07-24 18:29:17 -0300

    Adicionado modumo models que contem as classes utilizadas para abstrair objetos

---
+++ models.py
@@ -0,0 +1,10 @@
+import cv2
+
+class Image:
+	""" Classe usada como base para trabalhar com as imagens, 
+	abstrai as informacoes relevantes de uma imagm"""
+
+	def __init__(self, image_path):
+		self.path = image_path
+		self.label = image_path[-5]
+		self.image = cv2.imread(image_path)
